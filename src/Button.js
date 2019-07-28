8894dd3937466ded72bfcc4106d5d3bc2431882b
Rohit <rohit.r.baddam@gmail.com>
2019-07-28 18:26:55 -0400

    I at least got the calculator to Display. I will add funcitonality later

--- src/Button.js
+++ src/Button.js
@@ -1,67 +1,87 @@
 import React, {Component} from 'react'
+import Zero from "./assets/0.png";
+import One from "./assets/1.png";
+import Two from "./assets/2.png";
+import Three from "./assets/3.png";
+import Four from "./assets/4.png";
+import Five from "./assets/5.png";
+import Six from "./assets/6.png";
+import Seven from "./assets/7.png"
+import Eight from "./assets/8.png"
+import Nine from "./assets/9.png";
+import Flip from "./assets/flip.png"
+import Remainder from "./assets/remainder.png"
+import Divide from "./assets/divide.png"
+import Clear from "./assets/AC.png"
+import Add from "./assets/add.png"
+import Multiply from "./assets/multiply.png"
+import Subtract from "./assets/subtract.png"
+import Decimal from "./assets/decimal.png"
+import Equals from "./assets/equals.png"
+
 
 class Button extends Component {
     state = {
-        imgloc: ''
+        imgloc: Zero
     }
     calculateValue = () => {
         switch (this.props.val) {
             case "0":
-                this.setState({imgloc: "assets/0.png"});
+                this.setState({imgloc: Zero, id: "zero"});
                 break;
             case "1":
-                this.setState({imgloc: "assets/1.png"});
+                this.setState({imgloc: One});
                 break;
             case "2":
-                this.setState({imgloc: "assets/2.png"});
+                this.setState({imgloc: Two});
                 break; 
             case "3":
-                this.setState({imgloc: "assets/3.png"});
+                this.setState({imgloc: Three});
                 break;
             case "4":
-                this.setState({imgloc: "assets/4.png"});
+                this.setState({imgloc: Four});
                 break;
             case "5":
-                this.setState({imgloc: "assets/5.png"});
+                this.setState({imgloc: Five});
                 break;
             case "6":
-                this.setState({imgloc: "assets/6.png"});
+                this.setState({imgloc: Six});
                 break;
             case "7":
-                this.setState({imgloc: "assets/7.png"});
+                this.setState({imgloc: Seven});
                 break;
             case "8":
-                this.setState({imgloc: "assets/8.png"});
+                this.setState({imgloc: Eight});
                 break;
             case "9":
-                this.setState({imgloc: "assets/9.png"});
+                this.setState({imgloc: Nine});
                 break;
             case "x":
-                this.setState({imgloc: "assets/multiply.png"});
+                this.setState({imgloc: Multiply});
                 break;
             case "/": 
-                this.setState({imgloc: "assets/divide.png"});
+                this.setState({imgloc: Divide});
                 break;
             case "+":
-                this.setState({imgloc: "assets/add.png"});
+                this.setState({imgloc: Add});
                 break;
             case "-":
-                this.setState({imgloc: "assets/subtract.png"});
+                this.setState({imgloc: Subtract});
                 break; 
             case "AC":
-                this.setState({imgloc: "assets/clear.png"});
+                this.setState({imgloc: Clear});
                 break;
             case "flip":
-                this.setState({imgloc: "assets/flip.png"});
+                this.setState({imgloc: Flip});
                 break;
             case "remainder":
-                this.setState({imgloc: "assets/remainder.png"});
+                this.setState({imgloc: Remainder});
                 break;
             case "decimal":
-                this.setState({imgloc: "assets/decimal.png"});
+                this.setState({imgloc: Decimal});
                 break;
             case "equals":
-                this.setState({imgloc: "assets/equals.png"});
+                this.setState({imgloc: Equals});
                 break;
             default: 
                 break;
@@ -71,7 +91,7 @@ class Button extends Component {
         
         return (
             <div className="Button" onLoad = {() => this.calculateValue()}>
-                <img src={this.state.imgloc} alt={this.state.imgloc} />
+                <img src={this.state.imgloc} id={this.state.id} alt={this.state.imgloc} />
             </div>
         )
     }
--- src/Calculator.js
+++ src/Calculator.js
@@ -9,30 +9,42 @@ class Calculator extends Component {
 
     render() {
         return (
+            
             <div className="calculator">
-                <Button val="AC" />
-                <Button val="flip" />
-                <Button val="remainder" />
-                <Button val="/" />
+            <input type="text" id="topbar" /> <br />
+                <div className="Row">
+                    <Button val="AC" />
+                    <Button val="flip" />
+                    <Button val="remainder" />
+                    <Button val="/" />
+                </div>
                 <br />
-                <Button val="7" />
-                <Button val="8" />
-                <Button val="9" />
-                <Button val="x" />
+                <div className="Row"> 
+                    <Button val="7" />
+                    <Button val="8" />
+                    <Button val="9" />
+                    <Button val="x" />
+                </div>
                 <br />
+                <div className="Row"> 
                 <Button val="4" />
                 <Button val="5" />
                 <Button val="6" />
                 <Button val="-" />
+                </div>
                 <br />
+                <div className="Row"> 
                 <Button val="1" />
                 <Button val="2" />
                 <Button val="3" />
                 <Button val="+" />
+                </div>
                 <br />
+                <div className="Row"> 
                 <Button id="zero" val="0" />
                 <Button val="decimal" />
                 <Button val="equals" />
+                </div>
                 <br />
              </div>
         )
